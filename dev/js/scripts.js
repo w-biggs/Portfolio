4b60f584132ee8cc97a0520a478acb202cbf2ec0
wilsonbiggs <wilson@wilsonbiggs.com>
2018-08-10 14:15:55 -0400

    removing console logs from production

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
   $(".device").click(function(){
     if($(this).hasClass("current-device")){
-      console.log("not switched");
       return;
     } else {
       $(this).siblings(".current-device").removeClass("current-device");
       $(this).addClass("current-device");
-      console.log("switched");
     }
   })
 });
