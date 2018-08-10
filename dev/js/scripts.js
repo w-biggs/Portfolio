d3ee114ef61e09315b4512b92a0928346564aa86
wilsonbiggs <wilson@wilsonbiggs.com>
2018-08-10 14:05:33 -0400

    adding visual toggle functionality

---
+++ dev/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".device").click(function(){
+    if($(this).hasClass(".current-device")){
+      return;
+    } else {
+      $(this).siblings(".current-device").removeClass(".current-device");
+      $(this).addClass(".current-device");
+    }
+  })
+});
